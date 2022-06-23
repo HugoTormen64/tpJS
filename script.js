8128b86e6bac9bc47808018278d1331018903a31
syrine <syrina.derbel@hotmail.fr>
2022-06-23 16:20:11 +0200

    partie1 with function

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ let montantDeco = 0;
 let montantInitial = 0;
 montantDeco = parseInt(montantDeco);
 montantInitial = parseInt(montantInitial);
-wantDeco = prompt('Voulez-vous un découvert?');
-if (wantDeco == 'oui') {
+
+function decouvert() {
+    wantDeco = prompt('Voulez-vous un découvert?');
+    if (wantDeco == 'oui') {
+        do {
+            montantDeco = prompt('Choissisez votre montant de découvert :');
+            if (montantDeco < 100 || montantDeco > 2000) {
+                alert('Entrez un nouveau montant');
+            }
+        } while (montantDeco < 100 || montantDeco > 2000);
+        alert('Votre demande a été prise en compte');
+    }
     do {
-        montantDeco = prompt('Choissisez votre montant de découvert :');
-        if (montantDeco < 100 || montantDeco > 2000) {
+        montantInitial = prompt("Entrez votre montant initial :");
+        if (montantInitial < 500) {
             alert('Entrez un nouveau montant');
         }
-    } while (montantDeco < 100 || montantDeco > 2000);
-    alert('Votre demande a été prise en compte');
-}
-do {
-    montantInitial = prompt("Entrez votre montant initial :");
-    if (montantInitial < 500) {
-        alert('Entrez un nouveau montant');
-    }
-} while (montantInitial < 500);
+    } while (montantInitial < 500);
 
-alert("Votre découvert est : " + montantDeco + " \n Votre montant initial est : "+ montantInitial );
+    alert("Votre découvert est : " + montantDeco + " \n Votre montant initial est : " + montantInitial);
+}
+decouvert();
