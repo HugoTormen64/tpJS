4d8e1a6e35065fc5b419a708a4be2b28bf09c4b2
Nabil <nab.belbachir@gmail.com>
2022-06-24 08:49:29 +0400

    agios au format JS sans fonctions

--- script.js
+++ script.js
@@ -4,26 +4,32 @@ Le taux de banque = 10 / 100 = 0.1
 */
 
 const bankRate = 10/100;
+let ovdft =0, ovdftTme = 0;
 
 // Faire entrer le montant du découvert et Arrondir à 2 chiffres
-let ovdft = prompt("saisissez le montant du découvert autorisé utilisé");
-ovdft = parseFloat(ovdft);
-ovdftAm = ovdft.tofixed(2);
+ovdft = parseFloat(prompt("saisissez le montant du découvert autorisé utilisé"));
+let ovdftAm = ovdft.toFixed(2);
+console.log ("montant du découvert : "+ovdftAm);
 
-// Si montant = 0 : Affiche "Découvert non autorisé => pas d'agios"
-if (ovdftAm === 0) {
-    prompt("Découvert non autorisé => pas d'agios");
+
+// Si montant = 0 : Affiche "Découvert non autorisé => pas d'agios" puis "Ciao".
+if (ovdftAm == 0) {
+    alert("Découvert non autorisé => pas d'agios");
 }
-// Sinon Vérifier que le montant entré est commpris entre 100€ et 2000€
+else{
+// Sinon forcer à entrer un montant commpris entre 100€ et 2000€
+while (ovdftAm < 100 || ovdftAm >= 2000){
+    ovdftAm = prompt("Saisissez un montant compris entre 100 et 2000");
+}
+// Entrer la durée d'utilisation du découvert en jours
 do {
-    ovdft = prompt("saisissez le montant du découvert autorisé utilisé");
-} while (!(ovdftAm >= 100 && <= 2000));
+        ovdftTme = parseInt(prompt("Saisissez la durée du découvert autorisé utilisé"));
+} while (ovdftTme < 1 || ovdftTme > 366);
 
-// Faire entrer la durée d'utilisation du découvert en jours 
-do {
-    ovdftTme = prompt("saisissez le montant du découvert autorisé utilisé");
-} while (ovdftTme > 1 && < 366);
+// Calcul des agios arrondi à 2 décimales
+let agios = (ovdftTme * ovdftTme * bankRate) / 365;
+agios = agios.toFixed(2);
 
-let agios = ovdftTme * ovdftTme * bankRate / 365;
-
-prompt ("Le montant des agios est : ", agios);
+alert("Le montant des agios est : "+ agios+" €");
+}
+alert("hasta la vista !");
